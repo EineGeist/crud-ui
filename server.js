615934271f62be35de66295a474b49cf20381aa5
Ivan Dovzhikov <einegeist@gmail.com>
2020-04-27 12:04:59 +0300

    server: just a couple of semicolons and line breaks, nothing to look at

--- server.js
+++ server.js
@@ -2,27 +2,26 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 
-
 const { mongoURI: db } = require('./config/keys');
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
-
+  .catch(console.log);
 
 const server = express();
 server.use(express.json());
 server.use('/api/records', require('./routes/api/records'));
 
-
 if (process.env.NODE_ENV === 'production') {
-  server.use(express.static('client/build'))
+  server.use(express.static('client/build'));
 
   server.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {console.log(`Server is running on port ${PORT}...`)});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
