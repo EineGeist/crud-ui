764ffdaaa1c68c24996896d774ca3b472c23e7f2
Ivan Dovzhikov <einegeist@gmail.com>
2020-04-27 13:38:16 +0300

    server: refactor, add validation error handler

--- routes/api/records.js
+++ routes/api/records.js
@@ -3,35 +3,61 @@ const router = express.Router();
 
 const Record = require('../../models/Record');
 
+const handleError = err => {
+  let status, message;  
+
+  switch (err.name) {
+    case 'CastError':
+      status = 404;
+      message = { message: 'Not found' };
+      break;
+    case 'ValidationError':
+      const errors = [];
+      for (let failure in err.errors) {
+        const { path, kind, message } = err.errors[failure];
+        errors.push({ path, kind, message });
+      }
+      
+      status = 422;
+      message = { errors };
+      break;
+    default:
+      status = 500;
+      message = { message: 'Server error' };
+      console.log(err);
+  }
+
+  return [status, message];
+};
+
 router.get('/', async (req, res) => {
   try {
     const records = await Record.find();
     res.json(records);
   }
   
-  catch(err) {
-    res.status(500).json({ messege: 'Server error' });
+  catch (err) {
+    const [status, message] = handleError(err);
+    res.status(status).json(message);
   }
 });
 
 router.get('/:id', async (req, res) => {
-  const { params: { id } } = req;
+  const { id } = req.params;
 
   try {
     const record = await Record.findById(id);
     res.json(record);
   }
   
-  catch(err) {
-    if (err.name === 'CastError')
-      res.status(404).json({ messege: 'Not found' });
-    else
-      res.status(500).json({ messege: 'Server error' });
+  catch (err) {
+    const [status, message] = handleError(err);
+    res.status(status).json(message);
   }
 });
 
 router.post('/', async (req, res) => {
-  const { body: { data } } = req;
+  const { data } = req.body;
 
   try {
     const newRecord = new Record({ data });
@@ -39,13 +65,17 @@ router.post('/', async (req, res) => {
     res.json(result);
   }
   
-  catch(err) {
-    res.status(500).json({ messege: 'Server error' });
+  catch (err) {
+    const [status, message] = handleError(err);
+    res.status(status).json(message);
   }
 });
 
 router.put('/:id', async (req, res) => {
-  const { params: { id }, body: { data } } = req;
+  const {
+    params: { id },
+    body: { data },
+  } = req;
 
   try {
     const record = await Record.findById(id);
@@ -55,30 +85,26 @@ router.put('/:id', async (req, res) => {
 
     res.json(result);
   }
-
-  catch(err) {
-    if (err.name === 'CastError')
-      res.status(404).json({ messege: 'Not found' });
-    else
-      res.status(500).json({ messege: 'Server error' });
+  
+  catch (err) {
+    const [status, message] = handleError(err);
+    res.status(status).json(message);
   }
 });
 
 router.delete('/:id', async (req, res) => {
-  const { params: { id } } = req;
-  
+  const { id } = req.params;
+
   try {
     const record = await Record.findById(id);
 
     await record.remove();
-    res.json( {messege: "Success"} );
+    res.json({ message: 'Success' });
   }
-
-  catch(err) {
-    if (err.name === 'CastError')
-      res.status(404).json({ messege: 'Not found' });
-    else
-      res.status(500).json({ messege: 'Server error' });
+  
+  catch (err) {
+    const [status, message] = handleError(err);
+    res.status(status).json(message);
   }
 });
 
